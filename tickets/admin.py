92285a7c8562bfab209f7c8acc3990359f8e192a
missfavour97 <kemwilson574@icloud.com>
2026-05-02 10:56:22 +0300

    Refactor code structure for improved readability

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Ticket, Unit, Topic, Comment, Attachment, StatusHistory
+from .models import Ticket, Unit, Topic, Comment, StatusHistory
 
 admin.site.register(Ticket)
 admin.site.register(Unit)
 admin.site.register(Topic)
 admin.site.register(Comment)
-admin.site.register(Attachment)
 admin.site.register(StatusHistory)
